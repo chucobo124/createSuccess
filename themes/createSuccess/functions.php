beb2eab48797ed3ee2c5f90cdc472bacc3c2dfab
ChenLucas <chucobo5219@gmail.com>
2017-04-28 07:37:22 +0800

    Add floating animation component

--- themes/createSuccess/functions.php
+++ themes/createSuccess/functions.php
@@ -113,7 +113,7 @@ function createsuccess_scripts() {
 	wp_enqueue_script( 'createsuccess-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'createsuccess-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-	wp_enqueue_script( 'navigation_floating', get_template_directory_uri() . '/js/navigation_floating.js');
+	wp_enqueue_script( 'navigation_floating', get_template_directory_uri() . '/js/navigation_floating.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- themes/createSuccess/js/navigation_floating.js
+++ themes/createSuccess/js/navigation_floating.js
@@ -1,27 +1,60 @@
 $(function() {
     // Stick the #nav to the top of the window
     var nav = $('.main-navigation');
+    var subMenu = $('.sub-menu');
     var navHomeY = nav.offset().top;
+    var subMenuHomeY = subMenu.offset().top;
     var isFixed = false;
     var $w = $(window);
     $w.scroll(function() {
         var scrollTop = $w.scrollTop();
         var shouldBeFixed = scrollTop > navHomeY;
+        var subMenuShouldBeFixed = scrollTop > (subMenuHomeY + 10);
         if (shouldBeFixed && !isFixed) {
             nav.css({
                 position: 'fixed',
-                top: 0,
-                left: nav.offset().left,
-                width: nav.width()
+                margin: '0px 0px 0px 25px'
             });
             isFixed = true;
         }
         else if (!shouldBeFixed && isFixed)
         {
             nav.css({
-                position: 'static'
+                position: 'absolute',
+                margin: '25px 0px 0px 25px'
             });
             isFixed = false;
         }
+        else if( subMenuShouldBeFixed && shouldBeFixed){
+          nav.css({
+              margin: '20px 0px 0px 25px'
+          });
+          console.log(subMenu.offset().left);
+          subMenu.css({
+               top: 0,
+               margin: '0px 0px 0px '+subMenu.offset().left+ 'px',
+               position: 'fixed',
+          });
+        }
+        else if( !subMenuShouldBeFixed && shouldBeFixed ){
+          subMenu.css({
+               top: subMenuHomeY,
+               position: 'static',
+          });
+        }
+        // if (subMenuShouldBeFixed) {
+        //   subMenu.css({
+        //       top: '0px',
+        //       position: 'fixed',
+        //   });
+        //   nav.css({
+        //       position: 'fixed',
+        //       margin: '10px 0px 0px 25px'
+        //   });
+        // } else if(!subMenuShouldBeFixed){
+        //   subMenu.css({
+        //       position: 'absolute',
+        //   });
+        // }
     });
 });
